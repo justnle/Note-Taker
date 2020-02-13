71e697c34cbbdabbe0f4a1a94dafc22028a03f5c
Justin <twoplatinumchainz@gmail.com>
2020-02-13 15:37:16 -0800

    server setup

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+
+app.listen(PORT, () => console.log('App listening on PORT ' + PORT));
