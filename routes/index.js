fa474f4cafcd95f369d2aaa8a67870a99c60a08c
Justin <twoplatinumchainz@gmail.com>
2020-02-15 12:39:51 -0800

    removed commented code

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,6 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-// const router = express.Router();
-
-// router.use( function (req, res, next) {
-//     console.log('Initializing...');
-// });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
