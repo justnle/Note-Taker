3ef8df8973665c816425db14d6fc63b98213becf
Justin <twoplatinumchainz@gmail.com>
2020-02-15 21:38:08 -0800

    try router.use() later

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+// const router = express.Router();
+
+// router.use( function (req, res, next) {
+//     console.log('Initializing...');
+// });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
